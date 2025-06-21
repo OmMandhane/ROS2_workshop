51f1cdde365d18e69b4a90bb5d60dbff8dcbbc95
Om Mandhane <stillwake27@gmail.com>
2025-06-21 13:40:43 +0530

    restructured

---
+++ Day1/build/day1_cpp/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/om/ROS2_Workshop/Day1/src/day1_cpp
+# Build directory: /home/om/ROS2_Workshop/Day1/build/day1_cpp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Day1/build/new_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/om/ROS2_Workshop/Day1/src/new_robot_description
+# Build directory: /home/om/ROS2_Workshop/Day1/build/new_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
